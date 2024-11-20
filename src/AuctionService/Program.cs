ad639d5b08e6eb207fb1e5c849597eab2aa165ae
Andre Miranda Bernarde <ambernarde@gmail.com>
2024-11-20 19:26:36 -0300

    Add Auction as part of the parent repository

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,41 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using MassTransit;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AuctionDbContext>(opt => 
+{
+  opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    { 
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+  DbInitializer.InitDb(app);
+}
+catch (System.Exception e)
+{  
+  Console.WriteLine(e);
+} 
+
+app.Run();
