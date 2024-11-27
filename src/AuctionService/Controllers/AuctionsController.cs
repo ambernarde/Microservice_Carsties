d93f7783624e72f569d6eaf3449a778a250712ac
Andre Miranda Bernarde <ambernarde@gmail.com>
2024-11-27 12:02:02 -0300

    End of section 4

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -4,6 +4,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,10 +18,14 @@ public class AuctionsController : ControllerBase
   private readonly AuctionDbContext _context;
   private readonly IMapper _mapper;
 
-  public AuctionsController(AuctionDbContext context, IMapper mapper)
+  private readonly IPublishEndpoint _publishEndpoint;
+
+  public AuctionsController(AuctionDbContext context, IMapper mapper, 
+   IPublishEndpoint publishEndpoint)
   {
     _context = context;
     _mapper = mapper;
+    _publishEndpoint = publishEndpoint;
   }
 
   [HttpGet]
@@ -50,18 +56,22 @@ public class AuctionsController : ControllerBase
   [HttpPost]
   public async Task<ActionResult<AuctionDto>> CreateAuction (CreateAuctionDto auctionDto)
   {
-    var auction = _mapper.Map<Auction>(auctionDto);
-    //TODO : add current user as seller
-    auction.Seller = "test";
+      var auction = _mapper.Map<Auction>(auctionDto);
+      //TODO : add current user as seller
+      auction.Seller = "test";
 
-    _context.Auctions.Add(auction);
+      _context.Auctions.Add(auction);
 
-    var result = await _context.SaveChangesAsync() > 0;
+      var newAuction = _mapper.Map<AuctionDto>(auction);
 
-    if(!result) return BadRequest("Could not save changes to the DB");
+      await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
-    return CreatedAtAction(nameof(GetAuctionById),
-    new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+      var result = await _context.SaveChangesAsync() > 0;
+
+      if(!result) return BadRequest("Could not save changes to the DB");
+
+      return CreatedAtAction(nameof(GetAuctionById),
+      new {auction.Id},newAuction);
   }
 
 [HttpPut("{id}")]
@@ -79,6 +89,8 @@ public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAu
      auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
      auction.Item.Year    = updateAuctionDto.Year ?? auction.Item.Year;
 
+     await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
      var result = await _context.SaveChangesAsync() > 0 ;
 
      if(result) return Ok();
@@ -95,6 +107,8 @@ public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAu
 
     //todo check seller == username
     _context.Auctions.Remove(auction);
+
+    await _publishEndpoint.Publish<AuctionDeleted>(new {Id = auction.Id.ToString()});
 
     var result = await _context.SaveChangesAsync() > 0 ;
     if(!result) return BadRequest("Could not update DB");
--- src/SearchService/RequestHelpers/MappingProfiles.cs
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -7,7 +7,8 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-         CreateMap<AuctionCreated,Item>();           
+         CreateMap<AuctionCreated,Item>();
+         CreateMap<AuctionUpdated,Item>();           
     }
     
 }
